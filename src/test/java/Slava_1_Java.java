edd7371095f5f2e6be02dd34964ccc2f8585fd5d
Slava_Konovalov <slavakonovalov26@yandec.ru>
2025-04-11 22:02:05 +0300

    init commit

---
+++ src/test/java/Slava_1_Java.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class Slava_1_Java {
+    @Test
+    void myUdachniTests() {
+        open("https://yandex.by/");
+        $("[name=text]").setValue("selenide").pressEnter();
+        $("[id=search-result]").shouldHave(text("ru.selenide.org"));
+        closeWindow();
+
+        open("https://duckduckgo.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=r1-0]").shouldHave(text("https://selenide.org"));
+        closeWindow();
+    }
+}
